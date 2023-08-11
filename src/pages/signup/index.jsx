1528c5d0d99adb84406da4b849dd31f6965cbbab
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-08-11 20:53:42 +0500

    Create basic signup structure and api call.

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const SignupPage = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_DOMAIN}/signup`, {
+        user: formData,
+      });
+      
+      const accessToken = response.headers['access-token']; // Extract JWT token from response headers
+      
+      // Store the JWT token in session storage
+      sessionStorage.setItem('accessToken', accessToken);
+      
+      // Redirect to the root page
+      window.location.href = '/';
+    } catch (error) {
+      console.error('Signup error:', error);
+      // Handle error, possibly show an error message
+    }
+  };
+
+  useEffect(() => {
+    // If user is already authenticated (token exists), redirect to root
+    const accessToken = sessionStorage.getItem('accessToken');
+    if (accessToken) {
+      window.location.href = '/';
+    }
+  }, []);
+
   return (
     <div className="flex h-screen bg-gray-100">
       <div className="m-auto p-6 bg-white rounded-lg shadow-md">
         <h1 className="text-2xl font-semibold mb-4 text-gray-800">Sign Up</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label
               htmlFor="name"
@@ -17,6 +60,8 @@ const SignupPage = () => {
               type="text"
               id="name"
               name="name"
+              value={formData.name}
+              onChange={handleChange}
               className="w-full p-2 border rounded-md placeholder-gray-400"
               placeholder="Enter your name"
             />
@@ -32,6 +77,8 @@ const SignupPage = () => {
               type="email"
               id="email"
               name="email"
+              value={formData.email}
+              onChange={handleChange}
               className="w-full p-2 border rounded-md placeholder-gray-400"
               placeholder="Enter your email address"
             />
@@ -47,6 +94,8 @@ const SignupPage = () => {
               type="password"
               id="password"
               name="password"
+              value={formData.password}
+              onChange={handleChange}
               className="w-full p-2 border rounded-md placeholder-gray-400"
               placeholder="Choose a password"
             />
