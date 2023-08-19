21cd679d0e47fca6ff77a6789c5a9bb53aa70ef8
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-08-19 22:44:06 +0500

    load environment variables for base url.

---
+++ src/configs/apiEndpoints.js
@@ -0,0 +1,5 @@
+const apiEndpoints = {
+  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+};
+
+export default apiEndpoints;
--- src/configs/authConfig.js
+++ src/configs/authConfig.js
@@ -1,8 +1,10 @@
-export default {
-  baseUrl: "http://localhost:3001",
+const authConfig = {
+  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
   loginEndpoint: "/login",
   logoutEndpoint: "/logout",
   signUpEndpoint: "/signup",
   storageTokenKeyName: "accessToken",
   storageUserKeyName: "userData",
 };
+
+export default authConfig;
